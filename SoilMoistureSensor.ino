586f2dd99383b1d58bcd30b68f768dfb6a334e85
anjanapai2508 <anjanapai2508@users.noreply.github.com>
2019-03-23 12:48:12 -0700

    Arduino Code

---
+++ SoilMoistureSensor.ino
@@ -0,0 +1,28 @@
+const int analogInPin = A0;
+int sensorValue = 0;
+int relayOutput = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(relayOutput, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+sensorValue = analogRead(analogInPin);
+Serial.println(sensorValue);
+if(sensorValue < 300)
+{
+  digitalWrite(relayOutput, LOW); 
+  delay(5000);
+  digitalWrite(relayOutput, HIGH);
+}
+else
+{
+  digitalWrite(relayOutput, HIGH);
+ 
+  
+}
+delay(1000);     
+}
